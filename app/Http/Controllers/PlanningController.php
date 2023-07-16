9a820208eb62d01bcf220aa3b36e17e77ce50ce9
Marc Augier <m.augier@me.com>
2023-07-16 23:48:59 +0200

    Planning table creation

---
+++ app/Http/Controllers/PlanningController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Planning;
+use Illuminate\Http\Request;
+
+class PlanningController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Planning $planning)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Planning $planning)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Planning $planning)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Planning $planning)
+    {
+        //
+    }
+}
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
@@ -11,5 +12,9 @@ class Group extends Model
 
     public $timestamps = false;
     public $fillable = ['name', 'course_id'];
-
+    
+    public function plannings(): HasMany
+    {
+        return $this->HasMany(Planning::class);
+    }
 }
