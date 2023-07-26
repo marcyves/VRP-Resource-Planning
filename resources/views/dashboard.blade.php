852c6b4010dc74b2b056fa7e5aeb1549ae6f3297
Marc Augier <m.augier@me.com>
2023-07-27 00:19:29 +0200

    maybe useful

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,8 @@
         $gross_total_time = 0;   
         $gross_total_budget = 0;   
         @endphp
+        @isset($schools)
+            
         @foreach ($schools as $school)
         <x-advanced-course-table school="{{$school->name}}" school_id="{{$school->id}}">
             @php
@@ -17,43 +19,44 @@
              $total_budget = 0;   
             @endphp
             @foreach ($courses as $course)
-            @if($course->school_id==$school->id)
-            @php
-             $total_time += $course->session_length*$course->sessions;   
-             $total_budget += $course->rate*$course->session_length*$course->sessions;   
-            @endphp
-            <tr class="border-b">
-                <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">{{$course->name}}</th>
-                <td class="px-4 py-3">{{$course->year}}</td>
-                <td class="px-4 py-3">{{$course->semester}}</td>
-                <td class="px-4 py-3">{{$course->sessions}}</td>
-                <td class="px-4 py-3">{{$course->session_length}}</td>
-                <td class="px-4 py-3">{{$course->session_length*$course->sessions}}</td>
-                <td class="px-4 py-3">{{$course->rate}}</td>
-                <td class="px-4 py-3">{{$course->rate*$course->session_length*$course->sessions}}</td>
-                <td class="px-4 py-3 flex items-center justify-end">
-                    <form action="{{route('course.edit', $course->id)}}" method="get">
-                        <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-green-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
-                            Edit
-                        </button>    
-                    </form>
-                    <form action="{{route('course.destroy', $course->id)}}" method="post">
-                        @csrf
-                        @method('delete')
-                        <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-red-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
-                            Delete
-                        </button>    
-                    </form>
-                    <form action="{{route('course.show', $course->id)}}" method="get">
-                        @csrf
-                        <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-blue-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
-                            View
-                        </button>    
-                    </form>
-                </td>
-            </tr>
-            @endif                              
+                @if($course->school_id==$school->id)
+                @php
+                $total_time += $course->session_length*$course->sessions;   
+                $total_budget += $course->rate*$course->session_length*$course->sessions;   
+                @endphp
+                <tr class="border-b">
+                    <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">{{$course->name}}</th>
+                    <td class="px-4 py-3">{{$course->year}}</td>
+                    <td class="px-4 py-3">{{$course->semester}}</td>
+                    <td class="px-4 py-3">{{$course->sessions}}</td>
+                    <td class="px-4 py-3">{{$course->session_length}}</td>
+                    <td class="px-4 py-3">{{$course->session_length*$course->sessions}}</td>
+                    <td class="px-4 py-3">{{$course->rate}}</td>
+                    <td class="px-4 py-3">{{$course->rate*$course->session_length*$course->sessions}}</td>
+                    <td class="px-4 py-3 flex items-center justify-end">
+                        <form action="{{route('course.edit', $course->id)}}" method="get">
+                            <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-green-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
+                                Edit
+                            </button>    
+                        </form>
+                        <form action="{{route('course.destroy', $course->id)}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-red-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
+                                Delete
+                            </button>    
+                        </form>
+                        <form action="{{route('course.show', $course->id)}}" method="get">
+                            @csrf
+                            <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-blue-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
+                                View
+                            </button>    
+                        </form>
+                    </td>
+                </tr>
+                @endif                              
             @endforeach
+
             @php
                 $gross_total_time += $total_time;
                 $gross_total_budget += $total_budget;
@@ -72,6 +75,8 @@
             </tr>
         </x-advanced-course-table>
     @endforeach
+    @endisset
+
     <table class="w-full text-sm text-left text-gray-500">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
