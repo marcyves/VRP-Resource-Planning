c26c90c96e0fffe9ab0209fd05bbe0141a46dcad
Marc Augier <m.augier@me.com>
2023-07-30 01:11:36 +0200

    UI/UX

--- resources/views/course/edit.blade.php
+++ resources/views/course/edit.blade.php
@@ -1,12 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Course Update') }}
         </h2>
     </x-slot>
 
     <x-nice-box color="white">
-<h2>on modifie {{$course->id}}</h2>
 <form action="{{route('course.update', $course->id)}}" method="post">
     @csrf
     @method('put')
