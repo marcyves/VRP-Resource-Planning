de068b6348316fe7725d44cea4c1d4f8dfad7461
Marc Augier <m.augier@me.com>
2023-08-01 22:36:49 +0200

    Save selected semester and year in session

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -16,14 +16,22 @@ class SchoolController extends Controller
       
         if(isset($request->current_year)){
             $current_year = $request->current_year;
-        }else{
-         $current_year = now()->format('Y');
+            session(['current_year' => $current_year]);
+        }else {
+            $current_year = session('current_year');
+            if (!isset($current_year)) {
+                $current_year = now()->format('Y');
+            }
         }
 
         if(isset($request->current_semester)){
             $current_semester = $request->current_semester;
+            session(['current_semester' => $current_semester]);
         }else{
-         $current_semester = "all";
+            $current_semester = session('current_semester');
+            if (!isset($current_semester)) {
+                 $current_semester = "all";
+            }
         }
 
         $schools = Auth::user()->schools()->get();
