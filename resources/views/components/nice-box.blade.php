a7a92cf40ddadcedcc933cd062b5764b4ede768f
Marc Augier <m.augier@me.com>
2023-09-02 17:00:05 +0200

    give more space to calendar

--- resources/views/components/nice-box.blade.php
+++ resources/views/components/nice-box.blade.php
@@ -1,8 +1,6 @@
 @props(['color'])
-<div class="py-2">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-12xl mx-auto sm:px-6 lg:px-8 mt-2">
         <div class="bg-{{$color}} overflow-hidden shadow-sm sm:rounded-lg px-6 py-4">
         {{ $slot }}
         </div>
     </div>
-</div>
