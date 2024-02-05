891252adb2d717cecf9b7e885542e441725106a8
Marc Augier <m.augier@me.com>
2024-02-05 11:10:25 +0100

    UI/UX course and school management

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -68,14 +68,6 @@ class SchoolController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('school.create');
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
--- resources/views/school/index.blade.php
+++ resources/views/school/index.blade.php
@@ -6,8 +6,6 @@
         @if(Auth::user()->getMode() == "Edit")
         <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0  md:items-center justify-end md:space-x-3">
             <a class="p-2 text-sm border border-gray-300 rounded-md font-semibold font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" 
-            href="{{route('school.create')}}">{{ __('messages.school_create') }}</a>
-            <a class="p-2 text-sm border border-gray-300 rounded-md font-semibold font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" 
                 href="{{route('school.list')}}"> {{ __('messages.school_no_course') }}</a>
         </div>
         @endif
@@ -16,11 +14,43 @@
     <x-nice-box color="white">
         <ul>
             @foreach ($schools as $school)
-            <li class="mb-2 text-sm font-medium text-blue-800 hover:text-gray-800 rounded-lg focus:outline-none">
-                <a href="{{route('school.show', $school->id)}}">{{$school->name}}</a>
-            </li>       
+            <div class="mx-auto max-w-screen-xl px-2 lg:px-12">
+            <div class="bg-white relative shadow-md sm:rounded-lg overflow-hidden mb-4">
+                <div class="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
+                    <div class="w-full md:w-1/2">
+                    <a href="{{route('school.show', $school->id)}}">{{$school->name}}</a>
+                    </div>
+                    @if(Auth::user()->getMode() == "Edit")
+                    <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                        <div class="flex items-center space-x-3 w-full md:w-auto">
+                            <form action="{{route('school.edit', $school->id)}}" method="get">
+                                <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-green-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    </svg>  
+                                </button>    
+                            </form>
+                            <form action="{{route('school.destroy', $school->id)}}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="inline-flex items-center p-0.5 text-sm font-medium text-center text-red-500 hover:text-gray-800 rounded-lg focus:outline-none" type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                    </svg>                                  
+                                </button>    
+                            </form>
+                            <a
+                            class="inline-flex items-center p-2 text-sm border border-gray-300 rounded-md font-semibold font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none" 
+                            href="{{route('course.create', $school->id)}}">Add Course</a>
+                        </div>
+                    </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+
             @endforeach
-        </ul>
+        </ul>  
     </x-nice-box>
 
     <x-nice-box color="white">
