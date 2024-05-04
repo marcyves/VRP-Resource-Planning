0f9675d8b875cc07fd5096bfe81cda25156ccfdb
Marc Augier <m.augier@me.com>
2024-05-04 23:36:19 +0200

    switch management and admin views

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -10,6 +10,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Filament\Navigation\NavigationItem;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -53,6 +54,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->navigationItems([
+                NavigationItem::make('Retour VRP')
+                    ->url('/', shouldOpenInNewTab: false)
+                    ->icon('heroicon-c-arrow-left-start-on-rectangle')
             ]);
     }
 }
