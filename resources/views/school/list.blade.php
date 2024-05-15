d5bb3bfb4b7eb7d9f7d2f91d2dedcdfa0132843b
Marc Augier <m.augier@me.com>
2024-05-16 01:45:30 +0200

    UI clean up school views

--- resources/views/school/list.blade.php
+++ resources/views/school/list.blade.php
@@ -6,12 +6,14 @@
     </x-slot>
 
     <section  class="nice-page">
-    @foreach($schools as $school)
-        <div class="mx-auto max-w-screen-xl px-2 lg:px-12">
-            <div class="bg-white relative shadow-md sm:rounded-lg overflow-hidden mb-4">
-                <x-school-header :school_name=$school->name :school_id=$school->id/>
-            </div>
-        </div>
-    @endforeach        
+        <ul>
+            @foreach ($schools as $school)
+            <li class="mx-auto max-w-screen-xl px-2 lg:px-12 bg-white shadow-md sm:rounded-lg overflow-hidden mb-2
+            flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-2">
+                <x-school-header :school_name="$school->name" :school_id="$school->id"/>
+            </li>
+            @endforeach
+        </ul>  
     </section>
+
 </x-app-layout>
