adf94b05df264d642f4beacab51995c3c486c3e8
Marc Augier <m.augier@me.com>
2024-05-25 14:05:35 +0200

    UI Breadcrumbs

--- resources/views/layouts/breadcrumbs.blade.php
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -16,7 +16,14 @@
         </li>
         @endif
         @if (session('course') !== null)
-        > <li class="inline p-2">{{session('course')}}</li>
+        > <li class="inline p-2">
+        <form action="{{route('course.show', session('course_id'))}}" method="get" class="inline">
+            @csrf
+            <button class="inline-flex items-center p-0.5 text-sm font-medium hover:text-gray-400 focus:outline-none" type="submit">
+                {{session('course')}}
+            </button>    
+        </form>        
+        </li>
         @endif
     </ul>
 </nav>
