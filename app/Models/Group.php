83036219f3172bce95de6f590272806006b91011
Marc Augier <m.augier@me.com>
2024-05-28 01:30:42 +0200

    Link group to company

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -7,16 +7,29 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+use Illuminate\Support\Facades\Auth;
+
 class Group extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
-    public $fillable = ['name', 'short_name','course_id', 'size'];
+    public $fillable = ['name', 'short_name','course_id', 'size', 'company_id'];
     
     public function plannings(): HasMany
     {
         return $this->HasMany(Planning::class);
+    }
+
+    public function getCourses()
+    {
+//        $company = Auth::user()->getCompany();
+
+        return Course::select(['courses.*'])
+        ->join('group_course', 'course_id', '=', 'courses.id')
+        ->where('group_course.group_id', '=', $this->id)
+        ->orderBy('courses.name')
+        ->get();
     }
 
      /**
