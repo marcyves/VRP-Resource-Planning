8566a7e34b8c1bd3f52f854a7a4d4181b1b30d36
Marc Augier <m.augier@me.com>
2024-06-09 02:05:43 +0200

    Ready for online, solved vite problem

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,10 +11,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Until I find what is wrong with the build process -->
-        <link href="/build/assets/app.css" rel="stylesheet">
-        <script src="/build/assets/app.js" type='text/javascript'>
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
