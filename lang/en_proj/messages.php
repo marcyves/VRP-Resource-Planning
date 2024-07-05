ca8779f0730d475f6f35bd620169d1451d38f06f
Marc Augier <m.augier@me.com>
2024-07-05 02:36:13 +0200

    Tentative to create vocabulary for clients/projects

---
+++ lang/en_proj/messages.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'Name',
+    'short_name' => 'Short name',
+    'size' => 'size',
+    'description' => 'Description',
+    'update' => 'Update',
+
+    'planning'     => 'Planning',
+
+    'school' => 'Client',
+    'school_create' => 'Create Client',
+    'school_no_course' => 'Client without Project',
+    'schools' => 'Clients',
+    'schools_list' => 'List Clients',
+    'schools_empty_list' => 'List Clients without Project',
+
+    'course' => 'Phase',
+    'course_list' => 'Phase List',
+    'course_create' => 'Phase Creation',
+    'add_course' => 'Add Phase',
+
+    'program' => 'Project',
+    'programs' => 'Projects',
+    'program_create' => 'Create new Project',
+
+    'groups' => 'Groups',
+    'group_create' => 'Create Group',
+
+    'billing' => 'Billing',
+    'bills' => 'Bills',
+    'bill_id' => 'Bill #',
+    'bill_create' => 'New Bill',
+
+    'year' => 'Year',
+    'semester' => 'Semester',
+    'sessions' => 'Sessions',
+    'session_length' => 'Duration',
+    'time' => 'Time',
+    'total_time' => 'Total time',
+    'rate' => 'Rate',
+    'hour_rate' => 'Average Rate',
+    'gain' => 'Gain',
+    'total_gain' => 'Total Gain',
+    'billing' => 'Billing',
+    'time_worked' => 'Time Worked',
+    'hours' => 'Hours',
+    'monthly_gain' => 'Monthly Gain',
+
+];
