2df29d415509e4575d1fdb743fdf293adabebad1
Marc Augier <m.augier@me.com>
2024-07-08 12:09:34 +0200

    Nice links and icons on language panel

--- resources/views/vendor/translation/nav.blade.php
+++ resources/views/vendor/translation/nav.blade.php
@@ -4,6 +4,18 @@
 
     <ul class="flex-grow justify-end pr-2">
         <li>
+            <a href="{{ route('dashboard') }}" class="{{ set_active('') }}{{ set_active('/') }}">
+                @include('translation::icons.home')
+                {{ __('Retour VRP') }}
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('filament.admin.pages.dashboard') }}" class="{{ set_active('') }}{{ set_active('/admin') }}">
+                @include('translation::icons.admin')
+                {{ __('Admin Panel') }}
+            </a>
+        </li>
+        <li>
             <a href="{{ route('languages.index') }}" class="{{ set_active('') }}{{ set_active('/create') }}">
                 @include('translation::icons.globe')
                 {{ __('translation::translation.languages') }}
