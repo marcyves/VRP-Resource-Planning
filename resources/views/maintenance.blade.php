fe381cb11ed5caa50ebf1512432bdfb31ff850ec
Marc Augier <m.augier@me.com>
2024-11-14 13:49:04 +0100

    cleanup maintenance message

--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -1,13 +1,11 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    <div class="w-full md:w-1/2">
-        <h2 class="inline-flex font-semibold text-xl text-gray-800 mr-4">
+    <div class="maintenance-notice">
+        <h2>
             {{ __('Maintenance') }}
         </h2>
+        <p>The site is currently in maintenance, sorry for the inconvenience</p>
     </div>
-    <p>The site is currently in maintenance, sorry for the inconvenience</p>
-
-
-    
+   
 </x-guest-layout>
