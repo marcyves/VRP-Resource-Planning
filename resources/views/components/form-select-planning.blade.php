444c8f3b256bab42ec64c9f3e24a2fcec42e3589
Marc Augier <m.augier@me.com>
2025-02-02 19:01:13 +0100

    UI Select course on Agenda

--- resources/views/components/form-select-planning.blade.php
+++ resources/views/components/form-select-planning.blade.php
@@ -2,24 +2,24 @@
     <form action="{{ route('planning.create')}}" method="post" class="cool-box">
         @csrf
         <input type="date" name="date" value="{{$year}}-{{substr("0".$month,-2)}}-{{ $day }}" class="my-box">
+        <label>Select a Course: </label> 
         <select name="course" class="course-select my-box"  onchange="this.form.submit()">
-            <option value disabled selected>-- Select a Course --</option>
             @if($mode == 'selected')
-            <option disabled>{{$schools->name}}</option>
+            <optgroup label="{{$schools->name}}">
                 @foreach ($courses as $course)
-                    <option selected value="{{$course->id}}"> -({{ $course->program_name }}) {{$course->name}}</option>
+                    <option selected value="{{$course->id}}">({{ $course->program_name }}) {{$course->name}}</option>
                 @endforeach
             @elseif($mode == 'single')
-            <option disabled>{{$schools->name}}</option>  
+            <optgroup label="{{$schools->name}}"> 
                 @foreach ($courses as $course)
-                    <option value="{{$course->id}}"> -({{ $course->program_name }}) {{$course->name}}</option>
+                    <option value="{{$course->id}}">({{ $course->program_name }}) {{$course->name}}</option>
                 @endforeach
             @else
             @foreach($schools as $school)
-                <option disabled>{{$school->name}}</option>                            
+                <optgroup label="{{$school->name}} ">                          
                 @foreach ($courses as $course)
                     @if($school->id == $course->school_id)
-                    <option value="{{$course->id}}"> -({{ $course->program_name }}) {{$course->name}}</option>
+                    <option value="{{$course->id}}">({{ $course->program_name }}) {{$course->name}}</option>
                     @endif
                 @endforeach
             @endforeach
